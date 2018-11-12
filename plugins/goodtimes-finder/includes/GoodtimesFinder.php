75245ef111ac932ca207b9d6198d782d62515b9e
Jack Vo <jack.vo@learnosity.com>
2018-11-12 11:46:11 +1100

    Initial commit

---
+++ plugins/goodtimes-finder/includes/GoodtimesFinder.php
@@ -0,0 +1,46 @@
+<?php
+
+class GOFI_GoodtimesFinder extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'gofi-goodtimes-finder';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'goodtimes-finder';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * GOFI_GoodtimesFinder constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'goodtimes-finder', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new GOFI_GoodtimesFinder;
